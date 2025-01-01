941d15ce271741cf6d1ff3b8eda16ce23f8fc8b2
kim0hoon <gns9460@gmail.com>
2025-01-01 14:26:55 +0900

    미디어 파일 업로드 Repository 추가 #37

--- app/src/main/java/com/eighteen/eighteenandroid/data/repository/di/RepositoryModule.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/data/repository/di/RepositoryModule.kt
@@ -1,11 +1,13 @@
 package com.eighteen.eighteenandroid.data.repository.di
 
+import com.eighteen.eighteenandroid.data.repository.MediaFileRepositoryImpl
 import com.eighteen.eighteenandroid.data.repository.MessageRepositoryImpl
 import com.eighteen.eighteenandroid.data.repository.MyPageRepositoryImpl
 import com.eighteen.eighteenandroid.data.repository.SchoolRepositoryImpl
 import com.eighteen.eighteenandroid.data.repository.UserRepositoryImpl
 import com.eighteen.eighteenandroid.data.repository.mock.MockChatRepositoryImpl
 import com.eighteen.eighteenandroid.domain.repository.ChatRepository
+import com.eighteen.eighteenandroid.domain.repository.MediaFileRepository
 import com.eighteen.eighteenandroid.domain.repository.MessageRepository
 import com.eighteen.eighteenandroid.domain.repository.MyPageRepository
 import com.eighteen.eighteenandroid.domain.repository.SchoolRepository
@@ -39,4 +41,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindMyPageRepository(myPageRepositoryImpl: MyPageRepositoryImpl): MyPageRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindMediaFileRepository(mediaFileRepositoryImpl: MediaFileRepositoryImpl): MediaFileRepository
 }
